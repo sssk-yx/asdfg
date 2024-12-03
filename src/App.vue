763af717df19b4bd6fe7376629ce9b4a34d8fe49
skl <2056329229@qq.com>
2024-12-03 19:14:38 +0800

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+   <div id="app">
+   <!-- 原始的Vue logo和HelloWorld组件 -->
+   <img alt="Vue logo" src="./assets/logo.png">
+   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+   <!-- 新添加的Dashboard组件 -->
+   <Dashboard />
+   </div>
+  </template>
+  
+  <script>
+  // 导入HelloWorld组件
+  import HelloWorld from './components/HelloWorld.vue'
+  // 导入新创建的Dashboard组件
+  import Dashboard from './components/DashboardComponent.vue'
+  
+  export default {
+   name: 'App',
+   components: {
+   // 注册HelloWorld组件
+   HelloWorld,
+   // 注册Dashboard组件
+   Dashboard
+   }
   }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  </script>
+  
+  <style>
+  #app {
+     font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  </style>
+  
